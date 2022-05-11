3fb16617a2d6f3601c691255a55a3c1b49a66695
Raymond Xu <raymondxu@Raymonds-MacBook-Pro.local>
2022-05-10 23:14:40 -0700

    Updated backend

--- src/main/java/comp8031/WebSocket.java
+++ src/main/java/comp8031/WebSocket.java
@@ -3,12 +3,7 @@ package comp8031;
 import comp8031.model.*;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collector;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import javax.websocket.DecodeException;
@@ -21,53 +16,39 @@ import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint(value = "/websocket/{userName}", 
+@ServerEndpoint(value = "/websocket/device",
                 decoders = MessageDecoder.class,
                 encoders = MessageEncoder.class)
 public class WebSocket {
 
     private Session session;
     private static Set<WebSocket> connection = new HashSet<>();
-    private static Map<String, String> users = new HashMap<>();
+    static private final int QUORUM = 3;
+    private static List<String> users;
 
     public WebSocket() {
-        users = new HashMap<>();
+        users = new ArrayList<>(QUORUM);
     }    
       
     @OnOpen
-    public void onOpen(Session session, @PathParam("userName") String userName) {
+    public void onOpen(Session session) {
         this.session = session;
         connection.add(this);
-        users.put(session.getId(), userName); 
-        Message message = new Message();
-        message.setFrom("Server");
-        message.setTo(userName);
-        message.setContent(String.format("Welcomet %s join chat room", userName));
-        broadcaset(message, null);
+        users.add(session.getId());
+        System.out.printf("New User gets connected %s \n",session.getId());
     }
 
     private static void broadcaset(Message message, Session session) {
-        if(session == null) {
-            connection.forEach(connection -> {
-                try {
-                    System.out.println(message);
-                    connection.getSession().getBasicRemote().sendObject(message);
-                } catch (IOException | EncodeException exc) {
-                    System.out.println(exc.getLocalizedMessage());
-                }
-            });
-        } else {
-            try{
-                System.out.println(message);
-                connection.stream()
-                          .filter(c -> c.getSession() != session ).collect(Collectors.toList())
-                          .get(0)
-                          .getSession().getBasicRemote().sendObject(message);
-            }catch (IOException | EncodeException exc ) {
-                System.out.println(exc.getLocalizedMessage());
-            }
+        try{
+            System.out.println(message.getContent());
+            connection.stream()
+                    .collect(Collectors.toList())
+                    .get(0)
+                    .getSession().getBasicRemote().sendObject(message);
+        }catch (IOException | EncodeException exc ) {
+            System.out.println(exc.getLocalizedMessage());
         }
-
+        
     }
 
     public Session getSession() {
@@ -77,16 +58,8 @@ public class WebSocket {
     @OnMessage
     public void onMessage(String string, Session session) throws IOException, DecodeException {
         MessageDecoder messageDecoder = new MessageDecoder();
-        
-        String toUserName = null;
-        for(Map.Entry<String, String> entry: users.entrySet()){
-           if(!entry.getKey().equalsIgnoreCase(session.getId())){
-               toUserName = entry.getValue();
-           }
-        }
         Message message = messageDecoder.decode(string);
         broadcaset(message, session);
-        System.out.println(string);
     }
 
     @OnError
@@ -96,7 +69,9 @@ public class WebSocket {
 
     @OnClose
     public void onClose(Session session) {
-        System.out.println("Session cloused");
+        Message message = new Message(TransactionStatus.DeviceDisconnected.toString());
+        this.users.removeIf(user -> user.equals(session.getId())); 
+        broadcaset(message, session);
     }
 
 } 
--- src/main/java/comp8031/model/TransactionStatus.java
+++ src/main/java/comp8031/model/TransactionStatus.java
@@ -1,2 +1,17 @@
-package comp8031.model;public enum TransactionStatus {
+package comp8031.model;
+
+public enum TransactionStatus {
+    TransactionBegin("Transaction Begin"),
+    TransactionEnd("Transaction End"),
+    TransactionComlete("Transaction Complete"),
+    DeviceDisconnected("Device Disconnected");
+    private String name;
+    TransactionStatus(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
